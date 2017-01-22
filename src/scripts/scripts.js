9ea3479f9c1b07cd2ab085047c0c6cc73924d2bf
thomas159 <thomas159@gmail.com>
2017-01-22 20:47:18 +0100

    mobile menu and hero text left

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -23,39 +23,3 @@ $('.close').addClass("hide");//toggle this class
 $('.nav').removeClass("white");//toggle this class
 $(".c-hamburger").removeClass("hide");
 }); 
-
-
-
-
-
-
-
-// Sticky Header
-$(window).scroll(function() {
-
-    if ($(window).scrollTop() > 100) {
-        $('.nav').addClass('sticky');
-    } else {
-        $('.nav').removeClass('sticky');
-    }
-});
-
-// (function() {
-
-//   "use strict";
-
-//   var toggles = document.querySelectorAll(".c-hamburger");
-
-//   for (var i = toggles.length - 1; i >= 0; i--) {
-//     var toggle = toggles[i];
-//     toggleHandler(toggle);
-//   };
-
-//   function toggleHandler(toggle) {
-//     toggle.addEventListener( "click", function(e) {
-//       e.preventDefault();
-//       (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-//     });
-//   }
-
-// })();
