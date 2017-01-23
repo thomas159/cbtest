5fab71e9d7c75255c538e81470b38532d6aff00e
thomas159 <thomas159@gmail.com>
2017-01-23 15:41:46 +0100

    final product

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".border-menu").click(function() { 
+  $(".hamburger").click(function() { 
     $('#menubar').toggleClass("hide");//toggle this class
     $('.nav').addClass("white");//toggle this class
      $('.close').toggleClass("hide");//toggle this class
@@ -21,5 +21,28 @@ $(".cell").click(function(){
 $("#menubar").addClass("hide");
 $('.close').addClass("hide");//toggle this class
 $('.nav').removeClass("white");//toggle this class
-$(".c-hamburger").removeClass("hide");
+$(".hamburger").removeClass("is-active");
 }); 
+
+$(document).ready(function(){
+  $(".hamburger").click(function(){
+    $(this).toggleClass("is-active");
+  });
+});
+
+// Smooth Scrolling
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
